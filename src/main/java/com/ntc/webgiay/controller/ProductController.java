8428dc602b45281775b6a516d849c98aa5866d4e
MrChuan13121 <88259532+MrChuan13121@users.noreply.github.com>
2022-04-29 10:56:50 +0700

    edit products page

--- src/main/java/com/ntc/webgiay/controller/ProductController.java
+++ src/main/java/com/ntc/webgiay/controller/ProductController.java
@@ -12,8 +12,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -43,91 +41,37 @@ public class ProductController {
         List<Brand> brandsReputation = brandService.getBrandReputation();
         model.addAttribute("listBrandsReputation",brandsReputation);
 
-//        //Tìm kiếm sản phẩm
-//        List<Product> listProduct = productService.searchProducts(keyword);
-//        model.addAttribute("listProduct", listProduct);
-//        model.addAttribute("keyword",keyword);
+        //Tìm kiếm sản phẩm
 
-
-
-        int currentPage = page.orElse(1);
-        int sizePage = size.orElse(3);
+        int currentPage = page.orElse(1);//Trang hiển thị
+        int sizePage = size.orElse(8);//Kích thước sản phẩm trong 1 trang
 
         Pageable pageable = PageRequest.of(currentPage - 1,sizePage);
-        Page<Product> resultPage = null;
 
-        if (StringUtils.hasText(keyword) ){
-            resultPage = productService.searchProducts(keyword,pageable);
-            model.addAttribute("keyword",keyword);
-        }else{
-            resultPage = productService.findAll(pageable);
-        }
+        Page<Product> listProduct = productService.searchProduct(keyword,pageable);//Lấy các
+        model.addAttribute("listProduct", listProduct);
+        model.addAttribute("keyword",keyword);
 
-        int totalPages = resultPage.getTotalPages();
-        if( totalPages > 0){
-            int start = Math.max(1, currentPage -2);
-            int end = Math.min(currentPage + 2, totalPages);
-
-            if(totalPages > 5){
-                if( end == totalPages){
+        int totalPage = listProduct.getTotalPages();
+        if( totalPage > 0 ){
+            int start = Math.max(1,currentPage - 2);
+            int end = Math.min(currentPage + 2,totalPage);
+            if( totalPage > 5){
+                if( end == totalPage ){
                     start = end - 5;
                 }else if (start == 1){
                     end = start + 5;
                 }
             }
-            List<Integer> pageNumbers = IntStream.rangeClosed(start,end)
+            List<Integer> pageNumber = IntStream.rangeClosed(start,end)
                     .boxed()
                     .collect(Collectors.toList());
-            model.addAttribute("pageNumber", pageNumbers);
+            model.addAttribute("pageNumber", pageNumber);
         }
-        model.addAttribute("productPage",resultPage);
+
         return "category";
 
     }
-
-//
-//    @GetMapping("/products/paginated")
-//    public String showListProductsByPage(ModelMap model,
-//                                         @RequestParam(name = "keyword", required = false) String keyword,
-//                                         @RequestParam("page") Optional<Integer> page,
-//                                         @RequestParam("size") Optional<Integer> size){
-//        int currentPage = page.orElse(1);
-//        int pageSize = size.orElse(5);
-//        Pageable pageable = PageRequest.of(currentPage,pageSize);
-//        Page<Product> resultPage = null;
-//
-//        if (StringUtils.hasText(keyword)){
-//            resultPage = productService.findByNameContaining(keyword,pageable);
-//            model.addAttribute("keyword",keyword);
-//        }else{
-//            resultPage = productService.findAll(pageable);
-//        }
-//
-//        int totalPages = resultPage.getTotalPages();
-//        if( totalPages > 0){
-//            int start = Math.max(1, currentPage -2);
-//            int end = Math.min(currentPage + 2, totalPages);
-//
-//            if(totalPages > 5){
-//                if( end == totalPages){
-//                    start = end - 5;
-//                }else if (start == 1){
-//                    end = start + 5;
-//                }
-//            }
-//            List<Integer> pageNumbers = IntStream.rangeClosed(start,end)
-//                .boxed()
-//                .collect(Collectors.toList());
-//            model.addAttribute("pageNumber", pageNumbers);
-//        }
-//        model.addAttribute("productPage",resultPage);
-//
-//        //Lấy tên các thương hiệu
-//        List<Brand> brandsReputation = brandService.getBrandReputation();
-//        model.addAttribute("listBrandsReputation",brandsReputation);
-//
-//        return "category";
-//    }
 
 
 
